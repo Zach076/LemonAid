05df658db8a2bc04435f57b9a9d85448f447885f
mccaffk2 <mccaffk2@wwu.edu>
2018-06-10 15:36:50 -0700

    Add new file

---
+++ frontend/empty.php
@@ -0,0 +1,9 @@
+<div class="main">
+  <div class="message-history" id="message-history">
+	<div class="message">
+		<a class="message_username">System</a>
+		<span class="message_timestamp"><?php echo date("g:i A"); ?></span>
+		<span class="message_content">Welcome <?php echo $username; ?>!<br>Please select a board from the menu to get started</span>
+	</div>
+  </div>
+</div>
